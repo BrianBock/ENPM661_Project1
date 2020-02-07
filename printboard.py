b096beee1eedcf500a44aa2209896a81878bad03
Brian Bock <bbock@terpmail.umd.edu>
2020-02-07 17:13:37 -0500

    simplifed printboard with flatboard function

--- printboard.py
+++ printboard.py
@@ -16,16 +16,7 @@ def printboard(board):
 	# becomes: 
 	# 1 4 7 2 5 8 3 6 0
 
-	num_rows=len(board) #one side of the puzzle
-	num_cols=len(board)
-	i=0
-	flatboard=np.empty([num_cols*num_rows],int)#Create an empty array which will be the single line print out
-	for col in range (0,num_cols):
-		for row in range(0,num_rows):
-			flatboard[i]=int(board[row,col])
-			i+=1
-
-	flatboard_clean=str(flatboard)[1:-1]+"\n" #Clean up the list style brackets and add a line break
+	flatboard_clean=flatboard(board)+"\n"
 	if verbose is True:
 		print(flatboard_clean)
 	nodePathfile.write(flatboard_clean)
