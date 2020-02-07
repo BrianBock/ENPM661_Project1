c6f582533f315ce2adc072c495bcd3c7fad32c68
Brian Bock <bbock@terpmail.umd.edu>
2020-02-07 17:10:26 -0500

    created flatboard function

--- printboard.py
+++ printboard.py
@@ -30,3 +30,30 @@ def printboard(board):
 		print(flatboard_clean)
 	nodePathfile.write(flatboard_clean)
 
+
+
+
+
+def flatboard(board):
+	"""This function takes an 8 puzzle board and converts it to a linear string
+	"""
+	# Orignal board:
+	# 1 2 3
+	# 4 5 6
+	# 7 8 0
+	# becomes: 
+	# "1 4 7 2 5 8 3 6 0"
+
+	num_rows=len(board) #one side of the puzzle
+	num_cols=len(board)
+	i=0
+	flatboard=np.empty([num_cols*num_rows],int)#Create an empty array which will be the single line print out
+	for col in range (0,num_cols):
+		for row in range(0,num_rows):
+			flatboard[i]=int(board[row,col])
+			i+=1
+
+	flatboard_clean=str(flatboard)[1:-1] #Clean up the list style brackets
+
+	return flatboard_clean
+
