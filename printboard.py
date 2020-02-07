cdf50abb2bd2e9033f550261b99f4c93e088ede5
Brian Bock <bbock@terpmail.umd.edu>
2020-02-07 17:42:49 -0500

    added flatboard_spaceless

--- printboard.py
+++ printboard.py
@@ -31,6 +31,38 @@ def flatboard(board):
 
 
 
+
+
+
+def flatboard_spaceless(board):
+	"""This function takes an 8 puzzle board and converts it to a linear string
+	"""
+	# Orignal board:
+	# 1 2 3
+	# 4 5 6
+	# 7 8 0
+	# becomes: 
+	# "1 4 7 2 5 8 3 6 0"
+
+	num_rows=len(board) #one side of the puzzle
+	num_cols=len(board)
+	i=0
+	flatboard=""#np.empty([num_cols*num_rows],int)#Create an empty array which will be the single line print out
+	for col in range (0,num_cols):
+		for row in range(0,num_rows):
+			flatboard=flatboard+str(board[row,col])
+			i+=1
+
+	flatboard_clean=flatboard[1:-1] #Clean up the list style brackets
+
+	return flatboard_clean
+
+
+
+
+
+
+
 def printboard(board):
 	"""This function takes an 8 puzzle board and writes it to a file
 	"""
