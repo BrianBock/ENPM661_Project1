bce5ab080f8e23b6b7dad60dea159b4c8484876f
Brian Bock <bbock@terpmail.umd.edu>
2020-02-07 14:30:08 -0500

    Moved printboard function to it's own file

--- 8puzzle.py
+++ 8puzzle.py
@@ -5,6 +5,7 @@
 #import required packages
 import numpy as np
 import matplotlib
+from printboard import printboard
 
 verbose=False #Switch this to true for lots of print statements
 
@@ -21,28 +22,7 @@ print(goal)
 # Create nodePath.txt file, which will house all of the moves we make
 nodePathfile=open("nodePath.txt","a+")
 
-def printboard(board):
-	# Board gets written to file 
-	# 1 2 3
-	# 4 5 6
-	# 7 8 0
-	# becomes: 
-	# 1 4 7 2 5 8 3 6 0
 
-
-	num_rows=len(board)
-	num_cols=len(board)
-	i=0
-	flatboard=np.empty([num_cols*num_rows],int)#Create an empty array which will be the single line print out
-	for col in range (0,num_cols):
-		for row in range(0,num_rows):
-			flatboard[i]=int(board[row,col])
-			i+=1
-
-	flatboard_clean=str(flatboard)[1:-1]+"\n" #Clean up the list style brackets and add a line break
-	if verbose is True:
-		print(flatboard_clean)
-	nodePathfile.write(flatboard_clean)
 
 printboard(goal)
 
