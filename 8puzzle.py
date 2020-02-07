b5e0ed49bf7fdc790875b5c64c63d9b13b619ff9
Brian Bock <bbock@terpmail.umd.edu>
2020-02-07 14:27:44 -0500

    created printboard function to write nodePath to file

--- 8puzzle.py
+++ 8puzzle.py
@@ -6,6 +6,8 @@
 import numpy as np
 import matplotlib
 
+verbose=False #Switch this to true for lots of print statements
+
 # Set up board and goal
 height=3
 width=height #board must be square
@@ -17,7 +19,7 @@ print(board)
 print(goal)
 
 # Create nodePath.txt file, which will house all of the moves we make
-nodePathfile=open("nodePath.txt","w+")
+nodePathfile=open("nodePath.txt","a+")
 
 def printboard(board):
 	# Board gets written to file 
@@ -26,4 +28,27 @@ def printboard(board):
 	# 7 8 0
 	# becomes: 
 	# 1 4 7 2 5 8 3 6 0
+
+
+	num_rows=len(board)
+	num_cols=len(board)
+	i=0
+	flatboard=np.empty([num_cols*num_rows],int)#Create an empty array which will be the single line print out
+	for col in range (0,num_cols):
+		for row in range(0,num_rows):
+			flatboard[i]=int(board[row,col])
+			i+=1
+
+	flatboard_clean=str(flatboard)[1:-1]+"\n" #Clean up the list style brackets and add a line break
+	if verbose is True:
+		print(flatboard_clean)
+	nodePathfile.write(flatboard_clean)
+
+printboard(goal)
+
+
+
+
+
+
 
