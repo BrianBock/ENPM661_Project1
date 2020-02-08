8a800bba3925d7d916bfc455793224c827e15d67
Brian Bock <bbock@terpmail.umd.edu>
2020-02-08 11:00:11 -0500

    testing permutations

--- 8puzzle.py
+++ 8puzzle.py
@@ -11,7 +11,7 @@ import actions
 import math
 
 
-#verbose=True #set this to True for more print statements (may be slower)
+verbose=True #set this to True for more print statements (may be slower)
 
 
 # Set up board and goal
@@ -36,31 +36,34 @@ board_tracker=np.empty([total_permutations],dtype='object') #must be type object
 
 
 #for i in range(0,total_permutations,4):
-myboard=np.empty(4)
+myboard=np.empty(4,dtype='object')
 
+k=0
 
-def findAllPerms(board):
-	global k
-	k=0
+def findAllPerms(board,k):
 	#4 ways to move
-	myboard1=actions.MoveLeft(board)
-	print("Flatboard is: "+flatboard_spaceless(myboard1))
-	# myboard2=actions.MoveRight(board)
-	# myboard3=actions.MoveUp(board)
-	# myboard4=actions.MoveDown(board)
-	if(flatboard_spaceless(myboard1) in board_tracker): # board configuration is already saved. Move on
-		return
-	elif(myboard1 is flatboard_spaceless(goal)): #goal found
-		print("Goal found!")
-	else:
-		print("Adding board to list")
-		board_tracker[k]=flatboard_spaceless(myboard1) # board configuration is not saved yet. Save it
-		k=k+1
+	myboard[0]=actions.MoveLeft(board)
+	myboard[1]=actions.MoveRight(board)
+	myboard[2]=actions.MoveUp(board)
+	myboard[3]=actions.MoveDown(board)
+	for i in range(0,4):
+		print("Flatboard is: "+flatboard_spaceless(myboard[i]))
+		if(flatboard_spaceless(myboard[i]) in board_tracker): # board configuration is already saved. Move on
+			if verbose==True:
+				m=1
+				#print("Board configuration is already saved.")
+			#return
+		elif(myboard[i] is flatboard_spaceless(goal)): #goal found
+			print("Goal found!")
+		else:
+			print("Adding board to list")
+			board_tracker[k]=flatboard_spaceless(myboard[i]) # board configuration is not saved yet. Save it
+			k=k+1
+			findAllPerms(myboard[i],k)
 
 
 
-
-findAllPerms(initialboard)
+findAllPerms(initialboard,k)
 
 print(board_tracker)
 
